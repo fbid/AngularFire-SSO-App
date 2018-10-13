7184acdc17b7cbe6bb89090df4a573b103e025fa
fbid <fbid.dev@gmail.com>
2018-10-13 19:34:09 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
